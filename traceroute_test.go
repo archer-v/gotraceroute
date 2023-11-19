c5c6e00aa1b6c9fc9e7fe0fdbfa4e7a626931c13
archer-v <mandalorian-one@users.noreply.github.com>
2023-11-19 23:23:40 +0500

    Add a test to concurrent run

--- traceroute_test.go
+++ traceroute_test.go
@@ -3,10 +3,13 @@ package traceroute
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
 )
 
-var testHosts = []string{"google.com", "facebook.com", "starshiptroopers.dev", "msn.com", "bing.com", "8.8.8.8"}
+var testHosts = []string{"google.com", "starshiptroopers.dev", "8.8.8.8", "1.1.1.1", "yahoo.com"}
+
+// var testHosts = []string{"google.com", "facebook.com", "starshiptroopers.dev", "msn.com", "bing.com", "8.8.8.8"}
 var testErrHosts = []string{"aaaabbbcczzzzzz", "aaaabbbcczzzzzzdddd1.com", "266.266.266.266"}
 
 func TestRunBlock(t *testing.T) {
@@ -51,15 +54,56 @@ func TestRun3Error(t *testing.T) {
 	}
 }
 
-/*
 func TestRun4Concurrent(t *testing.T) {
-	chans := make([]chan Hop, len(testHosts))
+	channels := make([]chan Hop, len(testHosts))
+	results := make([][]Hop, len(testHosts))
+	var err error
+	ctx := context.Background()
+	options := Options{
+		MaxHops: 32,
+	}
+
+	var wg sync.WaitGroup
 
 	for i, h := range testHosts {
-		chans[i] = make(chan Hop)
-		out, err := Run(context.Background(), testHosts[0], Options{}, chans[i])
+		channels[i], err = Run(ctx, h, options)
+		var status string
+		if err != nil {
+			status = err.Error()
+		} else {
+			status = "started"
+		}
+		fmt.Printf("Traceroute %v to %v: %v\n", i+1, h, status)
+
+		if err != nil {
+			continue
+		}
+		//results[i] = make([]Hop, options.MaxHops)
+		results[i] = []Hop{}
+		var mu sync.Mutex
+
+		wg.Add(1)
+		go func(i int, host string, channel chan Hop) {
+			for hop := range channel {
+				//fmt.Printf("Traceroute %v: %v\n", i, hop.StringHuman())
+				mu.Lock()
+				results[i] = append(results[i], hop)
+				//results[i][hop.Step-1] = hop
+				mu.Unlock()
+			}
+			fmt.Printf("Traceroute %v to %v: finished\n", i+1, host)
+			wg.Done()
+		}(i, h, channels[i])
+	}
+
+	wg.Wait()
+
+	for i, r := range results {
+		fmt.Printf("Traceroute result %v to host %v:\n", i+1, testHosts[i])
+		for _, h := range r {
+			if h.Step > 0 {
+				fmt.Println(h.StringHuman())
+			}
+		}
 	}
 }
-
-
-*/
