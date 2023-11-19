f14d74e9738320bfa680e1740f12be66962a4744
archer-v <mandalorian-one@users.noreply.github.com>
2023-11-19 23:22:41 +0500

    Rework options initialization and add JSON output

--- cmd/gotraceroute.go
+++ cmd/gotraceroute.go
@@ -4,35 +4,35 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"github.com/aeden/traceroute"
+	"github.com/archer-v/gotraceroute"
 	"os"
 	"time"
 )
 
+var (
+	options       traceroute.Options
+	json          bool
+	jsonCompact   bool
+	jsonFormatted bool
+	host          string
+)
+
 func main() {
-	maxTTL := flag.Int("m", traceroute.DefaultMaxHops, `Set the max time-to-live (max number of hops) used in outgoing probe packets`)
-	startTTL := flag.Int("f", traceroute.DefaultStartTtl, `Set the first used time-to-live, e.g. the first hop`)
-	retries := flag.Int("q", 1, `Set the number of probes per hop`)
-	port := flag.Int("p", traceroute.DefaultPort, `Set source and destination port to use`)
-	timeout := flag.Duration("z", time.Millisecond*traceroute.DefaultTimeoutMs, "Waiting timeout in ms")
-	pSize := flag.Int("l", 0, `Packet length`)
-	jsonCompact := flag.Bool("j", false, "Out the result as a JSON in compact format")
-	jsonFormatted := flag.Bool("J", false, "Out the result as a JSON in pretty format")
+	flag.IntVar(&options.MaxHops, "m", traceroute.DefaultMaxHops, `Set the max time-to-live (max number of hops) used in outgoing probe packets`)
+	flag.IntVar(&options.StartTTL, "f", traceroute.DefaultStartTtl, `Set the first used time-to-live, e.g. the first hop`)
+	flag.IntVar(&options.Retries, "q", 1, `Set the number of probes per hop`)
+	flag.IntVar(&options.Port, "p", traceroute.DefaultPort, `Set source and destination port to use`)
+	flag.DurationVar(&options.Timeout, "z", time.Millisecond*traceroute.DefaultTimeoutMs, "Waiting timeout in ms")
+	flag.IntVar(&options.PayloadSize, "l", 0, `Packet length`)
+	flag.BoolVar(&jsonCompact, "j", false, "Out the result as a JSON in compact format")
+	flag.BoolVar(&jsonFormatted, "J", false, "Out the result as a JSON in pretty format")
 
 	flag.Parse()
-	json := *jsonCompact || *jsonFormatted
-	host := flag.Arg(0)
+	json = jsonCompact || jsonFormatted
+	host = flag.Arg(0)
 	if host == "" {
 		flag.Usage()
 		os.Exit(1)
-	}
-	options := traceroute.Options{
-		Retries:     *retries,
-		MaxHops:     *maxTTL,
-		StartTTL:    *startTTL,
-		Port:        *port,
-		Timeout:     *timeout,
-		PayloadSize: *pSize,
 	}
 
 	c, err := traceroute.Run(context.Background(), host, options)
@@ -44,27 +44,10 @@ func main() {
 
 	var lastHop traceroute.Hop
 	for hop := range c {
-		if hop.Step == 1 {
-			if json {
-				fmt.Printf("[")
-			} else {
-				fmt.Printf("traceroute to %v (%v), %v hops max, %v byte packet payload\n", host, hop.Dst.IP.String(), options.MaxHops, options.PayloadSize)
-			}
-		} else {
-			if json {
-				fmt.Printf(",")
-				if *jsonFormatted {
-					fmt.Println()
-				}
-			}
-		}
-		if json {
-			fmt.Printf(hop.StringJSON(*jsonFormatted))
-		} else {
-			fmt.Println(hop.StringHuman())
-		}
+		displayHop(hop)
 		lastHop = hop
 	}
+
 	if lastHop.Step != 0 {
 		if json {
 			fmt.Printf("]")
@@ -75,4 +58,26 @@ func main() {
 	}
 
 	os.Exit(2)
+}
+
+func displayHop(h traceroute.Hop) {
+	if h.Step == 1 {
+		if json {
+			fmt.Printf("[")
+		} else {
+			fmt.Printf("traceroute to %v (%v), %v hops max, %v byte packet payload\n", host, h.Dst.IP.String(), options.MaxHops, options.PayloadSize)
+		}
+	} else {
+		if json {
+			fmt.Printf(",")
+			if jsonFormatted {
+				fmt.Println()
+			}
+		}
+	}
+	if json {
+		fmt.Printf(h.StringJSON(jsonFormatted))
+	} else {
+		fmt.Println(h.StringHuman())
+	}
 }
